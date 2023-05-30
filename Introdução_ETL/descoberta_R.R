8c095fb11250b874e01f2db789a35d60282fe5f3
Alexciane Lima <alexcianeal@gmail.com>
2023-05-30 14:50:52 -0300

    Exerc.Descoberta

---
+++ Introdução_ETL/descoberta_R.R
@@ -0,0 +1,21 @@
+#descoberta
+
+pacman::p_load(funModeling, tidyverse) 
+
+
+
+#olhando os dados
+library(funModeling)
+library(tidyverse)
+
+glimpse(iris3)
+status(iris3)
+freq(iris3)
+plot_num(iris3)
+profiling_num(iris3)
+
+glimpse(morley)
+status(morley)
+freq(morley)
+plot_num(morley)
+profiling_num(morley)
